97e4d0cda43f27cf6a5092a7face54e559ee6a5e
marloesEJ <158572639+MarloesEJ@users.noreply.github.com>
2026-03-27 03:38:05 +0100

    basic next level logic

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Level completed! Loading next level...");
+            LoadNextLevel();
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("This is the last level.");
+        }
+    }
+
+}

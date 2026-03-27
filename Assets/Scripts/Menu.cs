602125e06861683d371c792738ca1a5ee5a0c519
marloesEJ <158572639+MarloesEJ@users.noreply.github.com>
2026-03-27 14:00:34 +0100

    Menu buttons

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+        Debug.Log("Exiting game...");
+        Application.Quit();
     }
 }
